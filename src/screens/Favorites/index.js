d14780c907f894eb8b67a0eaf5220809c235d357
Alisson Vieira <alissonvieira.github@gmail.com>
2020-11-13 23:57:19 -0300

    Finalização da tela de favoritos

--- src/screens/Favorites/index.js
+++ src/screens/Favorites/index.js
@@ -2,13 +2,33 @@ import React from 'react';
 
 import {
   Container,
-  Text
+  Header,
+  Title,
+  Card,
+  Scroll
 } from './styles';
+
+import LocalItem from '../../components/LocalItem';
 
 function Favorites() {
   return (
     <Container>
-      <Text>Favorites screen</Text>
+      <Header>
+        <Title>Seus lugares Favoritos</Title>
+      </Header>
+      
+      <Card>
+        <Scroll showsVerticalScrollIndicator={false}>
+          <LocalItem />
+          <LocalItem />
+          <LocalItem />
+          <LocalItem />
+          <LocalItem />
+          <LocalItem />
+          <LocalItem />
+          <LocalItem />
+        </Scroll>
+      </Card>
     </Container>
   );
 }
--- src/screens/Favorites/styles.js
+++ src/screens/Favorites/styles.js
@@ -2,11 +2,32 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center;
-  justify-content: center;
+  background-color: #9668cf;
 `;
 
-export const Text = styled.Text`
+export const Header = styled.View`
+  width: 100%;
+  max-width: 200px;
+  padding: 30px;
+  margin-bottom: 60px;
+  margin-top: 40px;
+`;
+
+export const Title = styled.Text`
   font-family: 'Roboto_700Bold';
   font-size: 18px;
+  color: #fff;
+`;
+
+export const Card = styled.View`
+  flex: 1;
+  background-color: #f7ecff;
+  border-top-right-radius: 20px;
+  border-top-left-radius: 20px;
+  padding: 10px;
+`;
+
+export const Scroll = styled.ScrollView`
+  margin-top: 20px;
+  margin-bottom: 30px;
 `;
