e558a94e99d8658faea602d42cdb957287937efe
Alisson Vieira <alissonvieira.github@gmail.com>
2020-11-14 00:34:34 -0300

    Finalização da tela de perfil do usuário

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -1,14 +1,58 @@
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
 
 import {
   Container,
-  Text
+  Header,
+  Title,
+  Card,
+  UserInfoBox,
+  UserProfilePhoto,
+  UserName,
+  UserAccountInfo,
+  UserMailLabel,
+  UserMail
 } from './styles';
 
+import { FontAwesome } from '@expo/vector-icons';
+import SessionButton from '../../components/SessionButton';
+
 function Profile() {
+  const navigation = useNavigation();
+
+  function handleLogout() {
+    navigation.reset({
+      routes: [{ name: 'Login' }]
+    });
+  }
+
   return (
     <Container>
-      <Text>Profile screen</Text>
+      <Header>
+        <Title>Suas informações Pessoais</Title>
+      </Header>
+
+      <Card>
+        <UserInfoBox>
+          <UserProfilePhoto>
+            <FontAwesome name="user-circle" size={130} color="#ccc" />
+          </UserProfilePhoto>
+          <UserName>Alisson Vieira</UserName>
+        </UserInfoBox>
+
+        <UserAccountInfo>
+          <UserMailLabel>
+            Endereço de e-mail
+          </UserMailLabel>
+          <UserMail>alissonvieira.sosvet@devAcademy.com</UserMail>
+        </UserAccountInfo>
+
+        <SessionButton 
+          text="ENCERRAR SESSÃO" 
+          onPress={handleLogout}
+        />
+      </Card>
+      
     </Container>
   );
 }
--- src/screens/Profile/styles.js
+++ src/screens/Profile/styles.js
@@ -2,11 +2,64 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center;
-  justify-content: center;
+  background-color: #9668cf;
 `;
 
-export const Text = styled.Text`
+export const Header = styled.View`
+  width: 100%;
+  max-width: 250px;
+  padding: 30px;
+  margin-bottom: 40px;
+  margin-top: 40px;
+`;
+
+export const Title = styled.Text`
   font-family: 'Roboto_700Bold';
   font-size: 18px;
+  color: #fff;
+`;
+
+export const Card = styled.View`
+  flex: 1;
+  background-color: #f7ecff;
+  border-top-right-radius: 20px;
+  border-top-left-radius: 20px;
+  padding: 20px;
+`;
+
+export const UserInfoBox = styled.View`
+  width: 100%;
+  align-items: center;
+`;
+
+export const UserProfilePhoto = styled.View`
+  width: 130px;
+  height: 130px;
+  background-color: #e5e5e5;
+  border-radius: 65px;
+  margin-top: -60px;
+`;
+
+export const UserName = styled.Text`
+  margin-top: 10px;
+  font-size: 18px;
+  font-family: 'Roboto_500Medium';
+  color: #222;
+`;
+
+export const UserAccountInfo = styled.View`
+  margin-top: 20px;
+  
+`;
+
+export const UserMailLabel = styled.Text`
+  font-family: 'Roboto_500Medium';
+  font-size: 16px;
+  color: #999;
+`;
+
+export const UserMail = styled.Text`
+  font-family: 'Roboto_400Regular';
+  font-size: 14px;
+  color: #c4c4c4;
 `;
